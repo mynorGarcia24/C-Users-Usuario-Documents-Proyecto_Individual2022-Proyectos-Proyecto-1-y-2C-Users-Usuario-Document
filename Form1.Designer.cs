096f644eda6ecc1cd3fd49982a5d0f87a227fb30
mynorGarcia24 <114190168+mynorGarcia24@users.noreply.github.com>
2022-09-22 16:12:22 -0600

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,97 @@
+﻿
+namespace AppMatrices0_1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.bntMatriz = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.listBox2 = new System.Windows.Forms.ListBox();
+            this.btnCalc = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // bntMatriz
+            // 
+            this.bntMatriz.Location = new System.Drawing.Point(44, 250);
+            this.bntMatriz.Name = "bntMatriz";
+            this.bntMatriz.Size = new System.Drawing.Size(260, 79);
+            this.bntMatriz.TabIndex = 0;
+            this.bntMatriz.Text = "Generar Matrices";
+            this.bntMatriz.UseVisualStyleBackColor = true;
+            this.bntMatriz.Click += new System.EventHandler(this.bntMatriz_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(21, 12);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(151, 212);
+            this.listBox1.TabIndex = 1;
+            // 
+            // listBox2
+            // 
+            this.listBox2.FormattingEnabled = true;
+            this.listBox2.Location = new System.Drawing.Point(503, 12);
+            this.listBox2.Name = "listBox2";
+            this.listBox2.Size = new System.Drawing.Size(158, 212);
+            this.listBox2.TabIndex = 2;
+            // 
+            // btnCalc
+            // 
+            this.btnCalc.Location = new System.Drawing.Point(401, 250);
+            this.btnCalc.Name = "btnCalc";
+            this.btnCalc.Size = new System.Drawing.Size(260, 79);
+            this.btnCalc.TabIndex = 3;
+            this.btnCalc.Text = "Generar Matrices";
+            this.btnCalc.UseVisualStyleBackColor = true;
+            this.btnCalc.Click += new System.EventHandler(this.btnCalc_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(699, 355);
+            this.Controls.Add(this.btnCalc);
+            this.Controls.Add(this.listBox2);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.bntMatriz);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button bntMatriz;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.ListBox listBox2;
+        private System.Windows.Forms.Button btnCalc;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AppMatrices0_1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void bntMatriz_Click(object sender, EventArgs e)
+        {
+            int[,] valores = new int[2, 2];
+            //Almacenar valores en la matriz 
+            valores[0, 0] = 100;
+            valores[0, 1] = 50;
+            valores[1, 0] = 60;
+            valores[1, 1] = 25;
+
+            //Insertar valores de la matriz al listBox
+            listBox1.Items.Add(valores[0, 0]);
+            listBox1.Items.Add(valores[0, 1]);
+            listBox1.Items.Add(valores[1, 0]);
+            listBox1.Items.Add(valores[1, 1]);
+        }
+
+        private void btnCalc_Click(object sender, EventArgs e)
+        {
+            string[,] Matriz_2 = new string[3, 2];
+            Matriz_2[0, 0] = "a";
+            Matriz_2[0, 1] = "1";
+            Matriz_2[1, 0] = "b";
+            Matriz_2[1, 1] = "2";
+            Matriz_2[2, 0] = "c";
+            Matriz_2[2, 1] = "3";
+
+            listBox2.Items.Add(Matriz_2[0, 0]);
+            listBox2.Items.Add(Matriz_2[1, 0]);
+            listBox2.Items.Add(Matriz_2[2, 0]);
+            listBox2.Items.Add(Matriz_2[0, 1]);
+            listBox2.Items.Add(Matriz_2[1, 1]);
+            listBox2.Items.Add(Matriz_2[2, 1]);
+
+        }
+    }
+}
